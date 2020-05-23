28f35dd5069654ca5ab827997fbfe40d92f5bae0
Evgenii Lartcev <i.dolende@gmail.com>
2020-05-23 19:38:22 +0300

    improved

--- App.py
+++ App.py
@@ -5,10 +5,15 @@ from PySide2 import QtCore, QtWidgets, QtGui
 import pigpio
 
 '''
- In early RC applications, the PWM signal min and max values were always between 1 and 2ms,
+Created on May 21, 2020
+@author: Evgenii_Lartcev
+
+Notes:
+
+- In early RC applications, the PWM signal min and max values were always between 1 and 2ms,
 and the frequency was 50Hz (period of 20ms). 
-a pulse of 1ms corresponds to zero throttle, and a pulse of 2ms to maximum throttle. 
-in pigpio library: param 'pulsewidth': The servo pulsewidth in microseconds. 0 switches pulses off.
+A pulse of 1ms corresponds to zero throttle, and a pulse of 2ms to maximum throttle. 
+In pigpio library: param 'pulsewidth': The servo pulsewidth in microseconds. 0 switches pulses off.
 '''
 
 
@@ -17,7 +22,6 @@ class MainWidget(QtWidgets.QWidget):
         QtWidgets.QWidget.__init__(self)
         self.setFixedSize(800, 600)
 
-        print("Init connection to pigpio daemon")
         self.esc_gpio_pin = 25
         self.piHost = "192.168.0.98"  # make sure on RaspberryPi "sudo pigpiod" was being run
         print("Init connection to pigpio daemon on " + self.piHost)
@@ -26,19 +30,19 @@ class MainWidget(QtWidgets.QWidget):
             print("Can't connect to " + self.piHost)
             exit()
         self.pi.set_servo_pulsewidth(self.esc_gpio_pin, 0)  # Starts (500-2500) or stops (0) servo pulses on the GPIO. We need to suppose that default frequency is 50 Hz.
+        print("Connection to pigpio daemon on " + self.piHost + " established, drive set to stop.")
 
         # in microseconds - ESC control by using old analog PWM style
         self.maxPulseWidth = 2000
         self.minPulseWidth = 700
 
-        self.escCalibrationButton = QtWidgets.QPushButton("Calibrate ESC")
-        # self.escCalibrationButton.adjustSize()
-        self.escCalibrationButton.clicked.connect(self.calibrate)
-
         self.escStopButton = QtWidgets.QPushButton("Stop drive and ESC.")
-        # self.escStopButton.adjustSize()
+        self.escStopButton.setMinimumWidth(150)
         self.escStopButton.clicked.connect(self.stop)
         self.driveStopped = False
+
+        self.escCalibrationButton = QtWidgets.QPushButton("Calibrate ESC")
+        self.escCalibrationButton.clicked.connect(self.calibrate)
 
         self.escSpeedControlSlider = QtWidgets.QSlider()
         self.escSpeedControlSlider.setOrientation(QtCore.Qt.Vertical)
@@ -46,18 +50,19 @@ class MainWidget(QtWidgets.QWidget):
         self.escSpeedControlSlider.setMaximum(self.maxPulseWidth) # should be 100
         self.escSpeedControlSlider.valueChanged.connect(self.manageEscSpeed)
 
-        self.debugLog = QtWidgets.QLabel("Hello!")
-        self.debugLog.setAlignment(QtCore.Qt.AlignRight)
+        self.debugLog = QtWidgets.QTextEdit("Connection to pigpio daemon on " + self.piHost + " established, drive set to stop.")
+        self.debugLog.setReadOnly(True)
+        self.debugLog.setMaximumHeight(200)
 
         self.main_layout = QtWidgets.QHBoxLayout()
 
         self.esc_layout = QtWidgets.QVBoxLayout()
-        # self.esc_layout.setAlignment(QtCore.Qt.AlignHCenter)
         self.esc_layout.addWidget(self.escSpeedControlSlider)
         self.esc_layout.addWidget(self.escStopButton)
         self.esc_layout.addWidget(self.escCalibrationButton)
 
         self.debug_layout = QtWidgets.QVBoxLayout()
+        self.debug_layout.setAlignment(QtCore.Qt.AlignBottom)
         self.debug_layout.addWidget(self.debugLog)
 
         self.main_layout.addLayout(self.esc_layout)
@@ -88,25 +93,25 @@ class MainWidget(QtWidgets.QWidget):
                     print("ESC has been successfully calibrated.")
                     self.debugLog.setText("ESC has been successfully calibrated. Restart program to run drive.")
         else:
-            self.debugLog.setText("Please stop the drive motion.")
+            self.debugLog.append("Please stop the drive motion.")
 
     # This will stop every action your Pi is performing for ESC or allow run for drive.
     def stop(self):
         if not self.driveStopped:
             self.pi.set_servo_pulsewidth(self.esc_gpio_pin, 0)
             self.pi.stop()
-            self.escStopButton.setText("ESC has been fully stopped.")
+            self.escStopButton.setText("ESC stopped.")
+            self.debugLog.append("ESC and drive has been fully stopped.")
             self.driveStopped = True
         else:
-            self.escStopButton.setText("ESC has been fully stopped already.")
-
+            self.debugLog.append(" and drive has been fully stopped already.")
 
     def manageEscSpeed(self):
         # self.debugLog.setText(str(self.escSpeedControlSlider.value())) #debug only
         if not self.driveStopped:
             self.pi.set_servo_pulsewidth(self.esc_gpio_pin, self.escSpeedControlSlider.value())
         else:
-            self.escStopButton.setText("ESC has been fully stopped already.")
+            self.debugLog.append("ESC has been fully stopped already.")
 
 
 if __name__ == "__main__":
